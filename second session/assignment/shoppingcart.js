836713b3b8f5da8447226a3535fb9de9f99d4fc0
Choi Minjune <minjune0807@gmail.com>
2024-04-15 04:02:09 +0900

    feat: 상품 클릭시 alert 생성 및 웹 스토리지 저장

--- second session/assignment/shoppingcart.js
+++ second session/assignment/shoppingcart.js
@@ -8,3 +8,16 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+
+// localStorage에서 상품 정보 접근
+var imgSrc = localStorage.getItem('productImgSrc');
+var imgAlt = localStorage.getItem('productImgAlt');
+var productName = localStorage.getItem('productName');
+var productPrice = localStorage.getItem('productPrice');
+
+// sessionStorage에서 상품 정보 접근
+var imgSrcSession = sessionStorage.getItem('productImgSrc');
+var imgAltSession = sessionStorage.getItem('productImgAlt');
+var productNameSession = sessionStorage.getItem('productName');
+var productPriceSession = sessionStorage.getItem('productPrice');
--- second session/assignment/shoppingmall.js
+++ second session/assignment/shoppingmall.js
@@ -12,3 +12,43 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const products = document.querySelectorAll('.product');
+
+    products.forEach(product => {
+        product.addEventListener('click', function() {
+            // confirm 대화 상자를 사용하여 사용자의 응답을 얻음
+            const isConfirmed = confirm('이 상품을 장바구니페이지에 추가하시겠습니까?');
+
+            // 사용자가 "확인"을 클릭했을 경우
+            if (isConfirmed) {
+               
+                var imgSrc = document.querySelector('.product img').src;
+                var imgAlt = document.querySelector('.product img').alt;
+                var productName = document.querySelector('.product h3').innerText;
+                var productPrice = document.querySelector('.product p').innerText;
+            
+                // localStorage에 저장
+                localStorage.setItem('productImgSrc', imgSrc);
+                localStorage.setItem('productImgAlt', imgAlt);
+                localStorage.setItem('productName', productName);
+                localStorage.setItem('productPrice', productPrice);
+            
+                // sessionStorage에 저장
+                sessionStorage.setItem('productImgSrc', imgSrc);
+                sessionStorage.setItem('productImgAlt', imgAlt);
+                sessionStorage.setItem('productName', productName);
+                sessionStorage.setItem('productPrice', productPrice);
+                
+                alert('상품이 장바구니페이지에 추가되었습니다.');
+            } else {
+                // 사용자가 "취소"를 선택했을 때 실행할 코드
+                alert('상품 추가가 취소되었습니다.');
+            }
+        });
+    });
+});
+
